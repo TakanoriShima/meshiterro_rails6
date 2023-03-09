3574254fb7ea48eb3291c8a40874435ade55fea1
Taro DMM <dmm@gmail.com>
2023-03-09 09:30:20 +0000

    完成

---
+++ app/models/post_image.rb
@@ -0,0 +1,13 @@
+class PostImage < ApplicationRecord
+  has_one_attached :image
+  belongs_to :user
+  has_many :post_comments, dependent: :destroy
+  
+  has_many :favorites, dependent: :destroy
+  validates :shop_name, presence: true
+  validates :image, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'], size: { between: 1.kilobyte..1.megabytes , message: '画像容量が大きすぎます。1MB以下にしてください。' }
+  
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end  
+end
---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  # ---- 下記1行を追加してください ---- #
+  has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  
+  has_many :favorites, dependent: :destroy
+  has_one_attached :profile_image
+  validates :name, presence: true
+  validates :profile_image, content_type: ['image/png', 'image/jpg', 'image/jpeg'], size: { between: 1.kilobyte..1.megabytes , message: '画像容量が大きすぎます。1MB以下にしてください。' }
+end
+  
